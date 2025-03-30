5bdf8fa892fc4453c4c656b92d1d646456501cd8
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-03-30 23:15:05 +0600

    Методы чтения данных 2 (read/retrieve)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,15 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        $post = Post::find(1);
-        dd($post);
+        // $posts = Post::all(); // Забрать все посты
+
+        $posts = Post::where('title', 'second post')->get(); // метод get() всегда возвращает массив даже если там 1 объект
+        
+        foreach($posts as $post){
+            dd($post->title); // вытащить поле title
+        }
 
     }
+
+
 }
