5dc3a98e849f3db69bc5b99713837ef4ef715a98
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-04-01 21:47:14 +0600

    View

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,13 +7,10 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        // $posts = Post::all(); // Забрать все посты
 
-        $posts = Post::where('title', 'second post')->get(); // метод get() всегда возвращает массив даже если там 1 объект
+        $posts = Post::all();
         
-        foreach($posts as $post){
-            dd($post->title); // вытащить поле title
-        }
+        return view('posts', compact( 'posts'));
 
     }
 
